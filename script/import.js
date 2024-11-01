42c75547e429a4e01b88dff0715a4cafdd6a4693
yamiyami3141 <yamiyami3141@gmail.com>
2024-11-01 17:39:23 +0900

    19

--- script/import.js
+++ script/import.js
@@ -1,6 +1,15 @@
 fetch("parts/footer.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#footer").innerHTML = data);
+
 fetch("forms/contact.html")
     .then((response) => response.text())
     .then((data) => document.querySelector("#forms").innerHTML = data);
+
+fetch("parts/loading.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector("#loading").innerHTML = data);
+
+fetch("parts/script.html")
+    .then((response) => response.text())
+    .then((data) => document.querySelector("#script").innerHTML = data);
