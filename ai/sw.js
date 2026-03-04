f19c43b5a1d8ce974c1814db05c3d95759ba4410
yamiyami3141 <yamiyami3141@gmail.com>
2026-03-04 16:27:25 +0900

    ai up

---
+++ ai/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'kotori-lab-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
